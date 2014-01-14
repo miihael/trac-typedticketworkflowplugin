1a450a70b80d095653701aee020b881378278de1
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2014-01-14 21:04:06 +0000

    0.2dev: Non-functional changes.  * PEP-0008 changes.  * Added license headers.

--- typedticketworkflowplugin/trunk/setup.py
+++ typedticketworkflowplugin/trunk/setup.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 Vladimir Abramov <kivsiak@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
 from setuptools import find_packages, setup
 
+
 setup(
-    name='TypedTicketWorkflow-plugin', version='0.1',
+    name='TracTypedTicketWorkflow',
+    version='0.2',
     packages=find_packages(),
-    author = 'Vladimir Abramov',
-    author_email = 'kivsiak@gmail.com',
-    licence = 'BSD',
-    description = 'Type - attribute workflow filter for trac ticket',
-    long_description = 'Allow to filter actions by \'type\' ticket attribute',
-    entry_points = {'trac.plugins':['typedworkflow.controller = typedworkflow.controller']},
+    author='Vladimir Abramov',
+    author_email='kivsiak@gmail.com',
+    licence='BSD',
+    description='Type - attribute workflow filter for trac ticket',
+    long_description='Allow to filter actions by \'type\' ticket attribute',
+    entry_points={
+        'trac.plugins': [
+            'typedworkflow.controller = typedworkflow.controller'
+        ]
+    },
 )
